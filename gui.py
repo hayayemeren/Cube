c6ce18cb41c29d29ce444bae5e7be0ce5a336725
Eren Türkmen <hayayemeren@gmail.com>
2025-04-16 17:52:32 +0300

    shuffle mode

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import subprocess
 
+
 def run_calibration():
     subprocess.run(["python3", "calibration.py"])
 
@@ -10,10 +11,12 @@ def run_visual_detection():
 def run_manual_input():
     subprocess.run(["python3", "manualinput.py"])
 
+def run_shuffle():
+    subprocess.run(["python3", "shuffle.py"])
 # Create the main window
 root = tk.Tk()
 root.title("Rubik's Cube Tools")
-root.geometry("300x200")
+root.geometry("200x300")
 
 # Create buttons
 btn_calibration = tk.Button(root, text="Run Calibration", command=run_calibration, height=2, width=20)
@@ -23,6 +26,9 @@ btn_visual_detection = tk.Button(root, text="Run Visual Detection", command=run_
 btn_visual_detection.pack(pady=10)
 
 btn_manual_input = tk.Button(root, text="Run Manual Input", command=run_manual_input, height=2, width=20)
+btn_manual_input.pack(pady=10)
+
+btn_manual_input = tk.Button(root, text="Shuffle", command=run_shuffle, height=2, width=20)
 btn_manual_input.pack(pady=10)
 
 # Run the GUI loop
--- shuffle.py
+++ shuffle.py
@@ -1,14 +1,21 @@
 import random
 import json
+from tkinter import messagebox
 
 motors = [1, 2, 3, 4, 5]
 times_turn = []
-shuffle_data = {}
+shuffle_data = {"Motor 1": None,
+                "Motor 2": None,
+                "Motor 3": None,
+                "Motor 4": None,
+                "Motor 5": None,}
 
 for motor in range(len(motors)):
 
     times_turn.append(random.randint(1,5))
-    shuffle_data[motor] = times_turn[motor]
+    shuffle_data[f"Motor {motor + 1}"] = times_turn[motor]
      
 with open("cube_shuffle.json", "w") as f:
                 json.dump(shuffle_data, f, indent=4)
+
+messagebox.showinfo("Shuffle Data", shuffle_data)
